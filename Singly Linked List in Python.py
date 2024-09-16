9949c8aad190f44f902b3de64aa537a5c6d46a33
kovendhan p <162816924+kovendhan5@users.noreply.github.com>
2024-09-16 07:19:49 +0530

    Singly Linked List in Python.py

--- Singly Linked List in Python.py
+++ Singly Linked List in Python.py
@@ -1,40 +1,39 @@
 class Node:
     def __init__(self, data):
-        self.data = data  # data node
-        self.next = None  # Pointer to next node (initially None)
-
+        self.data = data  
+        self.next = None  
 
 class LinkedList:
     def __init__(self):
-        self.head = None  # head of the linked list
+        self.head = None  
+    
 
-    # Add a new node at the end of the list
     def append(self, data):
-        new_node = Node(data)  # Create a new node with  data
-        if self.head is None:  # If the list is empty, set the new node as the head
+        new_node = Node(data)  
+
+        if self.head is None: 
             self.head = new_node
         else:
-            current = self.head  # Start from the head of the list
+            current = self.head 
+ 
             while current.next:  # Traverse to the last node
                 current = current.next
-            current.next = new_node  # Link the last node to the new node
-
+            current.next = new_node  
   
     def print_list(self):
-        current = self.head  # Start from the head node
+        current = self.head 
         while current:
-            print(current.data, end=" -> ")  # Print the current node's data
-            current = current.next  # Move to the next node
+            print(current.data, end=" -> ") 
+            current = current.next  
         print("None")  # End of the list
 
 
     def delete(self, data):
         current = self.head
 
-        # If the head node itself holds the key to be deleted
         if current and current.data == data:
-            self.head = current.next  # Change head to next node
-            current = None  # Free the old head
+            self.head = current.next  
+            current = None  # Free old head
             return
 
         # Search for the node to be deleted, keep track of the previous node
